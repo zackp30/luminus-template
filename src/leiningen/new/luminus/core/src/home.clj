e4d6bd74b4857de916795a61243704fda18f8df9
Zack Piper <zack.piper123@gmail.com>
2015-01-30 15:57:47 +0000

    Remove things that I don't need, for even better development!

--- src/leiningen/new/luminus/core/src/home.clj
+++ src/leiningen/new/luminus/core/src/home.clj
@@ -7,9 +7,6 @@
   (layout/render
     "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
 
-(defn about-page []
-  (layout/render "about.html"))
-
 (defroutes home-routes
   (GET "/" [] (home-page))
   (GET "/about" [] (about-page)))
